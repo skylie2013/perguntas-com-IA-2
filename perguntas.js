49728bd5ca4e04470725c68e26e8bb09624102d0
skylie2013 <104508830+skylie2013@users.noreply.github.com>
2024-09-04 08:04:07 -0300

    alteraçoes

--- perguntas.js
+++ perguntas.js
@@ -1,173 +1,41 @@
 export const perguntas = [
     {
-        enunciado: " ",
+        enunciado: "Atualmente o hábito de leitura não é comum entre os jovens, a leitura é algo importânte para a formação desses jovens?",
         alternativas: [
             {
-                texto: "",
-                afirmacao: [
-                    "",
-                    ""
-                ],
-                proxima: 1,
+                texto: "Sim, já que, além de inumeros livros servirem de base para vestibulares, eles podem trazer conhecimentos de assuntos diversos e expandir o vacabulario do leitor",
+                afirmacao: "Os livros ainda são de uma grande importância para os estudos e para a formação da etica e moral da população, sendo sem dúvidas um grande alicerce da sociedade."
             },
             {
-                texto: "",
-                afirmacao: [
-                    "",
-                    ""
-                ],
-                proxima: 2,
-            },
+                texto: "A leitura é indiferente, já que, atualmente não são necessarios livros para os estudos ou enterterimento, já que, atualmente não são necessarios livros para estudos levando em conta que hoje é possível encomtrar ambos nas redes sociais",
+                afirmacao: "Apesar do grande acesso a informaçõese conteúdos que as redes sociais nos proporcionam, os livros não deixam de ser essenciais na sociedade seja para estudos ou para obter conhecimento."
+            }
         ]
     },
     {
-        enunciado: "",
+        enunciado: "Algumas pessoas tem muita dificuldade para começarem no mundo da leitura, dito isso qual seria a maneira mais recomendada para se começar a ler?",
         alternativas: [
             {
-                texto: "",
-                afirmacao: [
-                    "",
-                    "",
-                    ""
-                ],
-                proxima: 3,
+                texto: "Ler livros pequenos, de facíl entendimento e que sejam de assuntos de interesse do leitor.",
+                afirmacao: "Para quem não  tem o hábito de ler, porém tem vontadede iniciar no mundo da leitura o mais indicado seria ler livros pequenos e de assuntos que sejam de interesse do leitor para despertar o interesse da leitura e tornar a mesma um habito."
             },
             {
-                texto: "",
-                afirmacao: [
-                    "",
-                    "",
-                    ""
-                ],
-                proxima: 4,
-            },
+                texto: "Ler livros didáticos e de autores conhecidos.",
+                afirmacao: "Para quem tem interessede começar na leitura não seria ideal começar diretamente pelos clássicos já que a leitura dos mesmos pode ser mais dificil dificultando assim a criação do habito da leitura."
+            }
         ]
     },
     {
-        enunciado: "",
+        enunciado: "Atualmente vemos um grande aumentos nos valores dos livros físicos, oque dificulta a aquisição dos mesmos, a leitura digital vem se popularizou entre os leitores nos ultimos anos em parte pelos preços mais acessiveis, então seria ela uma alternativa viavel aos livros físicos?",
         alternativas: [
             {
-                texto: "",
-                afirmacao: [
-                    "",
-                    "",
-                    ""
-                ],
-                proxima: 3,
+                texto: "Provavelmente, já que iria possibilitar uma maior aquisição por parte da população.",
+                afirmacao: "Já para quem não  tem condições de adquirir livros físicos os livros online são uma otima opção, pois permite a aquisição dos mesmos por um preço muito menor ou até mesmo gratuitamente, o que sem duvidas pode abrir grandes portas para esses leitores."
             },
             {
-                texto: "",
-                afirmacao: [
-                    "",
-                    "",
-                    ""
-                ],
-                proxima: 4,
-            },
+                texto: "Não teria grande importância já que grande parte da população continuaria adquirindo livros físicos apesar dos preços.",
+                afirmacao: "Apesar do consumo de livros físicos ainda ser maioria os livros oline permitem que pessoas que não podem adquirir livros físicos tenham acesso a leitura, podendo gerar grandes oportunidades para esses leitores."
+            }
         ]
     },
-    {
-        enunciado: "",
-        alternativas: [
-            {
-                texto: "",
-                afirmacao: [
-                    "",
-                    ""
-                ],
-                proxima: 5,
-            },
-            {
-                texto: "",
-                afirmacao: [
-                    "",
-                    ""
-                ],
-                proxima: 6,
-            },
-        ]
-    },
-    {
-        enunciado: "",
-        alternativas: [
-            {
-                texto: "",
-                afirmacao: [
-                    "",
-                    ""
-                ],
-                proxima: 5,
-            },
-            {
-                texto: "",
-                afirmacao: [
-                    "",
-                    ""
-                ],
-                proxima: 6,
-            },
-        ]
-    },
-    {
-        enunciado: "",
-        alternativas: [
-            {
-                texto: "",
-                afirmacao: [
-                    "",
-                    ""
-                ],
-                proxima: 7,
-            },
-            {
-                texto: "",
-                afirmacao: [
-                    "",
-                    ""
-                ],
-                proxima: 7,
-            },
-        ]
-    },
-    {
-        enunciado: "",
-        alternativas: [
-            {
-                texto: "",
-                afirmacao: [
-                    "",
-                    ""
-                ],
-                proxima: 7,
-            },
-            {
-                texto: "",
-                afirmacao: [
-                    "",
-                    ""
-                ],
-                proxima: 7,
-            },
-        ]
-    },
-    {
-        enunciado: "",
-        alternativas: [
-            {
-                texto: "",
-                afirmacao: [
-                    "",
-                    ""
-                ],
-            },
-            {
-                texto: "",
-                afirmacao: [
-                    "",
-                    "",
-                    ""
-                ],
-            },
-        ]
-    }
 ];
